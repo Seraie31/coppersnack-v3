f885a55da67d75027ad399758233fbbaca3509e3
Seraie31 <email.seraie31@hotmail.fr>
2025-12-09 08:36:12 +0100

    No code changes made

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-52f2a342'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.7qo3lgmijf8"
+    "revision": "0.jdl98lfh9bo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
